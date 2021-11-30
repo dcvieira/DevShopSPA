813977506455936cdef237a10f249bc4395cb006
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2021-11-30 11:56:56 -0300

    create app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'catalog', pathMatch: 'full' },
+  { path: 'catalog', loadChildren: () => import('./modules/catalog/catalog.module').then(m => m.CatalogModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
